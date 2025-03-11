5d3c899c70df8f6cf4a2e467733883443969dea7
Jiyada22 <jiyadakreenawong@gmail.com>
2025-03-11 20:27:05 +0700

    save

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -29,7 +29,7 @@ const upload = multer({ storage: storage });
 const upload2 = multer({ storage: storage2 });
 
 //const base_url = "http://localhost:3000";
-const base_url = "http://10.104.21.207";
+const base_url = "http://10.104.21.238";
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
